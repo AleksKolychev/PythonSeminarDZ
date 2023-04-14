af335d9bb71bdaf98922eb21b9a0e7f0fe6c294d
AleksKolychev <aleksushome@gmail.com>
2023-04-14 15:04:49 +0500

    Added standarts

--- Seminar2/Task10.py
+++ Seminar2/Task10.py
@@ -4,15 +4,30 @@
 # Выведите минимальное количество монет, которые нужно перевернуть
 
 
-print("Введите количество монет: ")
+# print("Введите количество монет: ")
+# n = int(input())
+# k = 0
+# print("Как лежат монеты? ")
+# for i in range(n):
+#     v = int(input())
+#     if v == 1:
+#         k += 1
+# if k < n / 2:
+#     print(f"Нужно перевертуть {k} монет")
+# else:
+#     print(f"Нужно перевертуть {n - k} монет")
+
+
 n = int(input())
-k = 0
-print("Как лежат монеты? ")
+count_zero = 0
+count_one = 0
 for i in range(n):
-    v = int(input())
-    if v == 1:
-        k += 1
-if k < n / 2:
-    print(f"Нужно перевертуть {k} монет")
+    x = int(input())
+    if x == 0:
+        count_zero += 1
+    else:
+        count_one += 1
+if count_one > count_zero:
+    print(count_zero)
 else:
-    print(f"Нужно перевертуть {n - k} монет")
+    print(count_one)
