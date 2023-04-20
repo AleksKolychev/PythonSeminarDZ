1ccde74757126a494f976c21cd9f3705058d4111
AleksKolychev <aleksushome@gmail.com>
2023-04-20 12:16:56 +0500

    Added standarts

--- Seminar4/Task24.py
+++ Seminar4/Task24.py
@@ -15,3 +15,16 @@ for i in range(n - 1):
     if (a[i] + a[i+1] + a[i-1] > x):
         x = a[i] + a[i+1] + a[i-1]
 print(f"Максимальное число ягод:{x}")
+
+
+# n = int(input())
+# a = list()
+# for i in range(n):
+#     x = int(input())
+#     a.append(x)
+
+# a_count = list()
+# for i in range(len(a) - 1):
+#     a_count.append(a[i - 1] + a[i] + a[i + 1])
+# a_count.append(a[-2] + a[-1] + a[0])
+# print(max(a_count))
